aedb0f1f06948b50745b127683398e676757bf09
Fowaliori <n1toleclown@gmail.com>
2025-02-25 19:40:59 +0700

    Ргз + чуть-чуть правки

--- src/main.js
+++ src/main.js
@@ -1,48 +1,73 @@
 $(document).ready(function() {
     let score = 0;
-    let totalClicks = 0;
+    let totalImages = 0;
     let testStarted = false;
-    let testDuration = 10000; // 10 секунд
     let interval;
+    let timer;
+    const showEndButtonAfter = 10;
 
     $('#startButton').click(function() {
         if (!testStarted) {
             testStarted = true;
             score = 0;
-            totalClicks = 0;
+            totalImages = 0;
             $('#result').text('');
+            $('#endButton').hide();
+            $('#timeElapsed').text(0);
+            startTimer();
             startTest();
-            setTimeout(endTest, testDuration);
         }
     });
+
+    $('#endButton').click(function() {
+        if (testStarted) {
+            endTest();
+        }
+    });
+
+    function startTimer() {
+        let elapsedTime = 0;
+        timer = setInterval(function() {
+            elapsedTime++;
+            $('#timeElapsed').text(elapsedTime);
+            if (elapsedTime >= showEndButtonAfter) {
+                $('#endButton').show();
+            }
+        }, 1000);
+    }
 
     function startTest() {
         interval = setInterval(function() {
             const img = $('<img class="image" src="../Photos/buggy.jpg" alt="target">');
-            const x = Math.random() * ($('#testArea').width() - 50);
-            const y = Math.random() * ($('#testArea').height() - 50);
+            const x = Math.random() * ($('#testArea').width() - 75);
+            const y = Math.random() * ($('#testArea').height() - 75);
             img.css({ left: x, top: y });
             $('#testArea').append(img);
-            img.fadeIn(200).delay(1000).fadeOut(200, function() {
+            img.fadeIn(200).delay(1500).fadeOut(200, function() {
                 $(this).remove();
             });
 
-            img.click(function() {
+            totalImages++;
+
+            img.click(function(event) {
+                event.stopPropagation();
                 score++;
-                totalClicks++;
                 $(this).addClass('exploded').fadeOut(200, function() {
                     $(this).remove();
                 });
             });
 
-        }, 2000);
+        }, 1600);
     }
 
     function endTest() {
         clearInterval(interval);
+        clearInterval(timer);
         testStarted = false;
+        $('#endButton').hide();
 
-        const successRate = (score / totalClicks) * 100;
-        $('#result').text(`Ваш результат: ${successRate.toFixed(2)}% удачных щелчков.`);
+        const successRate = (score / totalImages) * 100; // Процент успешных кликов
+        $('#result').text(`Ваш результат: ${successRate.toFixed(2)}% удачных щелчков. 
+        Всего изображений: ${totalImages} удачных кликов: ${score}`);
     }
 });
